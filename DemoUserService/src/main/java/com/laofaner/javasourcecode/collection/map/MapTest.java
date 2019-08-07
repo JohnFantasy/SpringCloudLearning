ae6c7d8c0721375f005ccb8ee780ef6ce170d5da
fanyuzhuo <fanyuzhuo@dooffe.com>
2019-08-07 14:46:19 +0800

    modify-验证修改map中的元素 add-增加一个读写文本文件的测试类

--- DemoUserService/src/main/java/com/laofaner/javasourcecode/collection/map/MapTest.java
+++ DemoUserService/src/main/java/com/laofaner/javasourcecode/collection/map/MapTest.java
@@ -15,10 +15,10 @@ import java.util.Map;
 public class MapTest {
 
     public static void main(String[] args) {
-        Map<String,Object> map = new HashMap<>();
-        map.put("a","b");
-        map.put("v","");
-        map.put("z",null);
+        Map<String, Object> map = new HashMap<>();
+        map.put("a", "b");
+        map.put("v", "");
+        map.put("z", null);
         System.out.println(map.get("v") == null);
 
         List<Long> list = new ArrayList<>();
@@ -28,5 +28,35 @@ public class MapTest {
 
         System.out.println(a.equals(902201002193426347L));
 
+        // 验证map中的value为引用类型时，若对对象进行修改，则map中的元素随之改变
+        Map<String, Student> map2 = new HashMap<>();
+        Student aaa = new Student("张三");
+        map2.put("a", aaa);
+        aaa.setName("李四");
+        System.out.println(map2.toString());
+
+    }
+}
+
+class Student {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                '}';
     }
 }
