eab2d93ffe7c84b289e9c0a245e808e980e0f6b5
fanyuzhuo <fanyuzhuo@dooffe.com>
2019-09-02 16:14:42 +0800

    add&modify-新增几个爬取网页的类；修改笔记

--- src/main/java/com/laofaner/javaFoundation/JavaIO/CrawHTML.java
+++ src/main/java/com/laofaner/javaFoundation/JavaIO/CrawHTML.java
@@ -22,33 +22,32 @@ public class CrawHTML {
      */
 
     public static void main(String[] args) {
-        CrawHTML crawHTML = new CrawHTML();
-        crawHTML.checkWebsite("https://www.baidu.com", "");
+        String html = CrawHTML.checkWebsite("https://www.ithome.com/");
+        System.out.println(html);
     }
 
-    public boolean checkWebsite(String checkUrl, String checkContent) {
+    private static String checkWebsite(String checkUrl) {
         try {
             URL url = new URL(checkUrl);
             HttpsURLConnection httpsConn = (HttpsURLConnection) url.openConnection();
             //取得该连接的输入流，以读取响应内容
             InputStream ins = httpsConn.getInputStream();
-            BufferedReader breader = new BufferedReader(new InputStreamReader(ins));
-            String info = breader.readLine();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(ins));
+            StringBuilder sb = new StringBuilder();
             long nowTime = new java.util.Date().getTime();
-            while (info != null) {
-                if (new java.util.Date().getTime() - nowTime > 10000)
-                    return false;//达到10秒就认为超时
-                if (info != null && info.indexOf(checkContent) != -1)
-                    return true;
-                info = breader.readLine();
+            while (reader.readLine() != null) {
+                String info = reader.readLine();
                 System.out.println(info);
+                if (new java.util.Date().getTime() - nowTime > 10000) {
+                    return "time out";
+                }//达到10秒就认为超时
+                sb.append(info);
             }
+            return sb.toString();
         } catch (Exception e) {
-            System.out.println("Can't get content:" + checkContent + " from URL:" + checkUrl);
             System.out.println("The error is:" + e.getMessage());
             e.printStackTrace();
-            return false;
+            return "false";
         }
-        return false;
     }
 }
