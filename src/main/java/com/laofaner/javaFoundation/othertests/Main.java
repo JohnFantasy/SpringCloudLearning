341d53389e23dbd493884315bdaa88114058296d
fanyuzhuo <fanyuzhuo@dooffe.com>
2019-10-14 11:45:14 +0800

    test-关于return和finally的测试

--- src/main/java/com/laofaner/javaFoundation/othertests/Main.java
+++ src/main/java/com/laofaner/javaFoundation/othertests/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         Main main = new Main();
         System.out.println(main.getValue());
-    }   
+    }
 
 
     public String getValue() {
