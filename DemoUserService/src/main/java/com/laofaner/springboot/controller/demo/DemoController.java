293b25c978d2f03802129b64ce73765b6727b913
fanyuzhuo <fanyuzhuo@dooffe.com>
2019-10-18 10:13:21 +0800

    add-学习使用spring框架提供的BeanUtils工具类来进行Java bean之间互转

--- DemoUserService/src/main/java/com/laofaner/springboot/controller/demo/DemoController.java
+++ DemoUserService/src/main/java/com/laofaner/springboot/controller/demo/DemoController.java
@@ -1,6 +1,9 @@
 package com.laofaner.springboot.controller.demo;
 
+import com.laofaner.commonutils.BeanConvertUtils;
 import com.laofaner.commonutils.CoordinateConvertion;
+import com.laofaner.springboot.domain.dto.TestDTO;
+import com.laofaner.springboot.domain.entity.TestEntity;
 import com.laofaner.springboot.domain.vo.Coornidate;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,7 +23,6 @@ import java.util.List;
 public class DemoController {
 
     @RequestMapping(value = "test", method = RequestMethod.POST)
-    @ResponseBody
     public String test1(@RequestBody Student student) {
 //        System.out.println(list.toString());
         System.out.println(student.toString());
@@ -28,16 +30,23 @@ public class DemoController {
     }
 
     @RequestMapping(value = "coordinateConvert", method = RequestMethod.POST)
-    @ResponseBody
     public Coornidate test1(@RequestBody Coornidate coornidate) {
         double longitude = coornidate.getLongitude().setScale(5, RoundingMode.HALF_UP).doubleValue();
         double latitude = coornidate.getLatitude().setScale(5, RoundingMode.HALF_UP).doubleValue();
         System.out.println(longitude);
-        String ret = CoordinateConvertion.wgs84togcj02(longitude,latitude);
+        String ret = CoordinateConvertion.wgs84togcj02(longitude, latitude);
         BigDecimal lng = new BigDecimal(Double.parseDouble(ret.split(",")[0]));
         BigDecimal lat = new BigDecimal(Double.parseDouble(ret.split(",")[1]));
         coornidate.setLongitude(lng);
         coornidate.setLatitude(lat);
         return coornidate;
+    }
+
+    /*
+    测试spring框架提供的类型转换工具BeanUtils
+     */
+    @RequestMapping(method = RequestMethod.POST, value = "beanConvertTest")
+    public TestDTO beanConvertTest(@RequestBody TestEntity testEntity) {
+        return BeanConvertUtils.convertUserEntityToDTO(testEntity);
     }
 }
---
+++ DemoUserService/src/main/java/com/laofaner/springboot/domain/dto/TestDTO.java
@@ -0,0 +1,19 @@
+package com.laofaner.springboot.domain.dto;
+
+import lombok.Data;
+
+/**
+ * @program: SpringBoot&SpringCloud
+ * @description: 测试类型转换工具BeanUtils的DTO
+ * @author: fyz
+ * @create: 2019-10-18 09:56
+ **/
+@Data
+public class TestDTO {
+
+    private String name;
+
+    private String address;
+
+    private String article;
+}
