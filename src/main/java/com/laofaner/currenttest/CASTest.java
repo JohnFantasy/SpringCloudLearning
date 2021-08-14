5f9a798036a6549071a12f5580ae5bdc2096d5a9
fanyuzhuo <306195115@qq.com>
2021-08-14 10:55:32 +0800

    add some test java files

---
+++ src/main/java/com/laofaner/currenttest/CASTest.java
@@ -0,0 +1,20 @@
+package com.laofaner.currenttest;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicStampedReference;
+
+/**
+ * @author fanyuzhuo
+ * @createAt 2021-07-18 15:18
+ * @description
+ */
+public class CASTest {
+
+    public static void main(String[] args) {
+        AtomicInteger atomicInteger  = new AtomicInteger();
+        System.out.println(atomicInteger.get());
+
+        AtomicStampedReference atomicStampedReference = new AtomicStampedReference(new Object(),0);
+
+    }
+}
